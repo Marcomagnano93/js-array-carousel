b61a32e32a988aed0a933f83305eee4561f68538
Marcomagnano93 <marcog.magnano@gmail.com>
2024-03-08 17:13:19 +0100

    ADD: interact

--- js/app.js
+++ js/app.js
@@ -8,9 +8,9 @@
     //imgArray [img1 img 2 img3 img4 img5]
     // i       [ 0     1     2    3    4 ]
 
-    let indexImg
+   
     //crea un ciclo for per scorrere le immagini
-    
+
     for(i = 0; i < imgArray.length; i++){
         // console.log(imgArray[i])
 
@@ -27,32 +27,56 @@
             <img src="${imgSelected}" alt="">
         </div>
         `
-        
-        
-      
-    }
+        console.log(indexImg)
 
-
-    const appear = document.querySelector(".item");
+        const appear = document.querySelector(".item");
 
         appear.classList.add("d-block");
-    
-    
-    const rightInteraction = document.querySelector(".right")
-    
-  
-// 2- Tutte le immagini saranno nascoste, tranne la prima, che avrà una classe specifica che la renderà visibile.
-    
+        
 
-   
-
-// 3- Al termine di questa fase ci ritroveremo con lo stesso slider stilato nella milestone 1,
-// ma costruito dinamicamente attraverso JavaScript.
-
-   
-    
+    }
 //Al click dell'utente sulle frecce, 
 //il programma cambierà l’immagine attiva, che quindi verrà visualizzata al posto della precedente.
+
+        const rightInteraction = document.querySelector(".right");
+
+        rightInteraction.addEventListener("click",
+        function(){
+
+            const allItems = document.getElementsByClassName("item");
+
+            for(index = 0; i < allItems.length; index++){
+
+                let selector = index;
+                
+                
+
+            }
+
+
+        })
+   
+// 2- Tutte le immagini saranno nascoste, tranne la prima, che avrà una classe specifica che la renderà visibile.
+// 3- Al termine di questa fase ci ritroveremo con lo stesso slider stilato nella milestone 1,
+// ma costruito dinamicamente attraverso JavaScript.
+    
+
+       
+    
+
+    
+    
+  
+
+    
+
+   
+
+
+
+   
+    
+
     
     
     
