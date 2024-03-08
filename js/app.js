aaf225a294675a54856f0ef8ce081f2f507acc97
Marcomagnano93 <marcog.magnano@gmail.com>
2024-03-08 14:02:13 +0100

    ADD: html skeleton

---
+++ js/app.js
@@ -0,0 +1,5 @@
+// 1- Adesso rimuoviamo tutto il markup statico e inseriamo tutte le immagini dinamicamente servendoci 
+// dell'array fornito e un semplice ciclo for che concatena un template literal.
+// 2- Tutte le immagini saranno nascoste, tranne la prima, che avrà una classe specifica che la renderà visibile.
+// 3- Al termine di questa fase ci ritroveremo con lo stesso slider stilato nella milestone 1,
+// ma costruito dinamicamente attraverso JavaScript.
