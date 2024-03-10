2d178297c4c9bd2093824a8557cb4521c0851dc0
Marcomagnano93 <marcog.magnano@gmail.com>
2024-03-10 10:31:44 +0100

    FIX: js corousel

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 // 1- Adesso rimuoviamo tutto il markup statico e inseriamo tutte le immagini dinamicamente servendoci 
 // dell'array fornito e un semplice ciclo for che concatena un template literal.
-
+// 2- Tutte le immagini saranno nascoste, tranne la prima, che avrà una classe specifica che la renderà visibile.
+// 3- Al termine di questa fase ci ritroveremo con lo stesso slider stilato nella milestone 1,
+// ma costruito dinamicamente attraverso JavaScript.
     
     //metti tutte le immagini in un array
 
@@ -16,54 +18,151 @@
 
     
 
-    let selector = 0; //number
-    let classes = ''; //string
+    
+    
    
 
+    
+        // const rightInteraction = document.querySelector(".right");
 
-        for(i = 0; i < imgArray.length; i++){
+        // rightInteraction.addEventListener("click",
+        // function(){
+
+        //     selector = selector + 1; //number
+
+        //     if(selector === 1){
+        //         classes = "d-block"; //string
+        //     }
+
+        // })
+
+
+        let selector = 0; //number
+        let classes = ''; //string
+
+        for(let i = 0; i < imgArray.length; i++){
             // console.log(imgArray[i])
-            
-
-            selector = selector++; //number
-            if(selector === 0){
-                classes = "d-block"; //string
-            }
          
             
             let indexImg = i; //number
             let imgSelected = imgArray[indexImg]; //function
-    
             
-    
+            if(i === 0){
+                classes = "d-block";
+            }
+            else{
+                classes = "";
+            }
+
             displayElement.innerHTML += `
-            <div class="item no-visual ${classes}">
-                <img src="${imgSelected}" alt="">
-            </div>
+                <div class="item ${classes}">
+                    <img src="${imgSelected}" alt="">
+                </div>
             `
-            const rightInteraction = document.querySelector(".right");
-            rightInteraction.addEventListener("click",
-                function() {
+           
         
-                    
-                    
-                  
-        
-        
-                })
         }
 
 
+
+
+        const rightInteraction = document.querySelector(".right");
+        rightInteraction.addEventListener("click",
+        function(){
+            
+                    
+                    if(selector < 4){
+                        selector++;//number
+                    }
+                    else{
+                        selector = 0;//number
+                    }
+
+                    displayElement.innerHTML = ""
+
+                for(let i = 0; i < imgArray.length; i++){
+                    // console.log(imgArray[i])
+                
+                    
+                    let indexImg = i; //number
+                    let imgSelected = imgArray[indexImg]; //function
+                    
+                    if(i === selector){
+                        classes = "d-block";
+                    }
+                    else{
+                        classes = "";
+                    }
+
+                    displayElement.innerHTML += `
+                        <div class="item ${classes}">
+                            <img src="${imgSelected}" alt="">
+                        </div>
+                    `
+                
+                
+                }
+        })
         
 
 
+        const leftInteraction = document.querySelector(".left");
+        leftInteraction.addEventListener("click",
+        function(){
+            
+                    
+                    if(selector > 0){
+                        selector--;//number
+                    }
+                    else{
+                        selector = 4;//number
+                    }
+
+                    displayElement.innerHTML = ""
+
+                for(let i = 0; i < imgArray.length; i++){
+                    // console.log(imgArray[i])
+                
+                    
+                    let indexImg = i; //number
+                    let imgSelected = imgArray[indexImg]; //function
+                    
+                    if(i === selector){
+                        classes = "d-block";
+                    }
+                    else{
+                        classes = "";
+                    }
+
+                    displayElement.innerHTML += `
+                        <div class="item ${classes}">
+                            <img src="${imgSelected}" alt="">
+                        </div>
+                    `
+                
+                
+                }
+        })
 
 
-            // 2- Tutte le immagini saranno nascoste, tranne la prima, che avrà una classe specifica che la renderà visibile.
-// 3- Al termine di questa fase ci ritroveremo con lo stesso slider stilato nella milestone 1,
-// ma costruito dinamicamente attraverso JavaScript.
+    // const rightInteraction = document.querySelector(".right");
+            // rightInteraction.addEventListener("click",
+            //     function() {
+                  
+        
+        
+            //     })
 
 
+          
+
+
+
+            //  if(selector === 1){
+            //      classes = "d-block"; //string
+            //  }
+
+            // })
 
 
 
